1bcd8ca178614d361d924fc708a00bc66be17fac
gducsn <gducsn@gmail.com>
2022-03-16 10:06:55 +0100

    add: cards and assets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CardsNavbarComponent } from './cards-navbar/cards-navbar.component';
+import { CardsComponent } from './cards/cards.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardsNavbarComponent
+    CardsNavbarComponent,
+    CardsComponent
     
   ],
   imports: [
