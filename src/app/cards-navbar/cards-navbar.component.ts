fe69fbab5825d63b5419bb737329b02de38330aa
nevromantik <jessicamoretti55@gmail.com>
2022-03-21 10:21:32 +0100

    final-navbar-card

--- src/app/cards-navbar/cards-navbar.component.ts
+++ src/app/cards-navbar/cards-navbar.component.ts
@@ -24,56 +24,115 @@ export class CardsNavbarComponent implements OnInit {
      if(this.btnStyle == "btn-change") {
        this.btnStyle ="btn-default";
      }else{
-       this.btnStyle = "btn-change"
+       this.btnStyle = "btn-change";
+       this.btnStyle2 = "btn-default";
+       this.btnStyle3 = "btn-default";
+       this.btnStyle4 = "btn-default";
+       this.btnStyle5 = "btn-default";
+       this.btnStyle6 = "btn-default";
+       this.btnStyle7 = "btn-default";
+       this.btnStyle8 = "btn-default";
+
+
      }
    }
    submit2(){
      if(this.btnStyle2 === "btn-change") {
        this.btnStyle2 ="btn-default";
      }else{
-       this.btnStyle2 = "btn-change"
+       this.btnStyle2 = "btn-change";
+       this.btnStyle = "btn-default";
+       this.btnStyle3 = "btn-default";
+       this.btnStyle4 = "btn-default";
+       this.btnStyle5 = "btn-default";
+       this.btnStyle6 = "btn-default";
+       this.btnStyle7 = "btn-default";
+       this.btnStyle8 = "btn-default";
+
      }
    }
    submit3(){
      if(this.btnStyle3 === "btn-change") {
        this.btnStyle3 ="btn-default";
      }else{
-       this.btnStyle3 = "btn-change"
+       this.btnStyle3 = "btn-change";
+       this.btnStyle = "btn-default";
+       this.btnStyle2 = "btn-default";
+       this.btnStyle4 = "btn-default";
+       this.btnStyle5 = "btn-default";
+       this.btnStyle6 = "btn-default";
+       this.btnStyle7 = "btn-default";
+       this.btnStyle8 = "btn-default";
      }
    }
    submit4(){
      if(this.btnStyle4 === "btn-change") {
        this.btnStyle4 ="btn-default";
      }else{
-       this.btnStyle4 = "btn-change"
+       this.btnStyle4 = "btn-change";
+       this.btnStyle = "btn-default";
+       this.btnStyle3 = "btn-default";
+       this.btnStyle2 = "btn-default";
+       this.btnStyle5 = "btn-default";
+       this.btnStyle6 = "btn-default";
+       this.btnStyle7 = "btn-default";
+       this.btnStyle8 = "btn-default";
      }
    }
    submit5(){
      if(this.btnStyle5 === "btn-change") {
        this.btnStyle5 ="btn-default";
      }else{
-       this.btnStyle5 = "btn-change"
+       this.btnStyle5 = "btn-change";
+       this.btnStyle = "btn-default";
+       this.btnStyle3 = "btn-default";
+       this.btnStyle4 = "btn-default";
+       this.btnStyle2 = "btn-default";
+       this.btnStyle6 = "btn-default";
+       this.btnStyle7 = "btn-default";
+       this.btnStyle8 = "btn-default";
      }
    }
    submit6(){
      if(this.btnStyle6 === "btn-change") {
        this.btnStyle6 ="btn-default";
      }else{
-       this.btnStyle6 = "btn-change"
+       this.btnStyle6 = "btn-change";
+       this.btnStyle = "bt-default";
+       this.btnStyle3 = "btn-default";
+       this.btnStyle4 = "btn-default";
+       this.btnStyle5 = "btn-default";
+       this.btnStyle2 = "btn-default";
+       this.btnStyle7 = "btn-default";
+       this.btnStyle8 = "btn-default";
      }
    }
    submit7(){
      if(this.btnStyle7 === "btn-change") {
        this.btnStyle7 ="btn-default";
      }else{
-       this.btnStyle7 = "btn-change"
+       this.btnStyle7 = "btn-change";
+       this.btnStyle = "bt-default";
+       this.btnStyle3 = "btn-default";
+       this.btnStyle4 = "btn-default";
+       this.btnStyle5 = "btn-default";
+       this.btnStyle6 = "btn-default";
+       this.btnStyle2 = "btn-default";
+       this.btnStyle8 = "btn-default";
      }
    }
    submit8(){
      if(this.btnStyle8 === "btn-change") {
        this.btnStyle8 ="btn-default";
      }else{
-       this.btnStyle8 = "btn-change"
+       this.btnStyle8 = "btn-change";
+       this.btnStyle = "bt-default";
+       this.btnStyle3 = "btn-default";
+       this.btnStyle4 = "btn-default";
+       this.btnStyle5 = "btn-default";
+       this.btnStyle6 = "btn-default";
+       this.btnStyle7 = "btn-default";
+       this.btnStyle2 = "btn-default";
      }
    }
 }
