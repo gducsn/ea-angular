8e6450b7a3b443d742b4c591d5b82906294a0a95
gducsn <gducsn@gmail.com>
2022-05-10 14:05:34 +0200

    add sign-in page form

--- src/app/routing/app-routing.module.ts
+++ src/app/routing/app-routing.module.ts
@@ -2,6 +2,7 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { NotFoundComponent } from "../components/not-found/not-found.component";
+import { SigninFormComponent } from "../components/signin-form/signin-form.component";
 import { HomeComponent } from "../home/home.component";
 
 
@@ -9,11 +10,10 @@ import { HomeComponent } from "../home/home.component";
 const routes: Routes = [
 { path: 'it-it', component: HomeComponent, data: {title: 'Home Page Electronic Arts - Sito Ufficiale'} },
 { path: '', redirectTo: '/it-it', pathMatch: 'full' },
+{ path: 'signin', component: SigninFormComponent, data: {title: 'Crea Account'}  },
 {path: '404', component: NotFoundComponent, data: {title: '404 - Official EA Site'}},
-  {path: '**', redirectTo: '/404'} ];
-
-
-
+  {path: '**', redirectTo: '/404'},
+  ];
 
 
 
